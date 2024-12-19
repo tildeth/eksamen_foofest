11066b797da26ac71e7e95b7a83f04bfaaa8fa0c
Tilde Twisttmann <tihe001@stud.kea.dk>
2024-12-19 13:42:35 +0100

    css

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { useBooking } from "@/context/BookingContext";
 import { FaShoppingCart } from "react-icons/fa"; 
 import styles from '@/styles/Cart.module.css'; 
+import Link from "next/link";
 
 const Cart = () => {
   const { totalTickets } = useBooking();
 
   return (
     <div className={styles.cartContainer}>
+      <Link href="/billet">
       <FaShoppingCart className={styles.cartIcon} />
       {totalTickets > 0 && (
         <div className={styles.ticketCount}>
           {totalTickets}
         </div>
       )}
+      </Link>
     </div>
   );
 };
